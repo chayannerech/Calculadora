2a52e1e63ec5e18eeb90349a13e97ce75a0a7f5b
chayannerech <rechchayanne@gmail.com>
2024-03-19 15:19:29 -0300

    Refaturação do código.

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -1,48 +1,75 @@
 ﻿using System.Diagnostics;
-
 namespace Calculadora.ConsoleApp
 {
-
     internal class Program
     {
         static void Main(string[] args)
         {
-
             while (true)
             {
-                decimal resultado = 0;
-                Console.Clear();
-                Console.WriteLine("Calculadora TopShow\n");
-                Console.WriteLine("Digite: \n 1 - para somar \n 2 - para subtrair \n 3 - para multiplicar \n 4 - para dividir \n S - para sair");
-                string operacao = Console.ReadLine();
+                string operacao = MostrarMenu();
 
-                if (operacao == "S" || operacao == "s")break;
-                if (operacao != "1" && operacao != "2" && operacao != "3" && operacao != "4") Console.WriteLine("\nOpção inválida. Por favor, tente novamente");
-                else
-                {
-                    Console.WriteLine("Digite o primeiro número");
-                    string primeiroNumeroString = Console.ReadLine();
-                    decimal primeiroNumero = Convert.ToDecimal(primeiroNumeroString);
+                #region Resto do código
+                if (SaidaSelecionada(operacao)) break;
+                if (OpcaoInvalida(operacao)) Console.WriteLine("\nOpção inválida. Por favor, tente novamente");
+                else RealizarCalculo(operacao);
 
-                    Console.WriteLine("Agora, o segundo número");
-                    string segundoNumeroString = Console.ReadLine();
-                    decimal segundoNumero = Convert.ToDecimal(segundoNumeroString);
-
-                    if (operacao == "4" && segundoNumero == 0) Console.WriteLine("\nNão é possível dividir um número por zero :( Por favor, tente novamente");
-                    else
-                    {
-                        if (operacao == "1") resultado = primeiroNumero + segundoNumero;
-                        else if (operacao == "2") resultado = primeiroNumero - segundoNumero;
-                        else if (operacao == "3") resultado = primeiroNumero * segundoNumero;
-                        else if (operacao == "4") resultado = primeiroNumero / segundoNumero;
-                        Console.WriteLine("\nShow! Seu resultado é: " + resultado);
-                    }
-                }
                 Console.WriteLine("Deseja continuar? [S/N]");
                 string continuar = Console.ReadLine();
-
                 if (continuar == "N" || continuar == "n") break;
-            }    
+                #endregion
+            }
+        }
+        static string MostrarMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Calculadora TopShow\n");
+            Console.WriteLine("Digite: \n 1 - para somar \n 2 - para subtrair \n 3 - para multiplicar \n 4 - para dividir \n S - para sair");
+            string operacao = Console.ReadLine();
+
+            return operacao;
+        }
+        static bool SaidaSelecionada(string opcao)
+        {
+            bool saidaSelecionada = opcao == "S" || opcao == "s";
+            return saidaSelecionada;
+        }
+        static bool OpcaoInvalida(string opcao)
+        {
+            bool opcaoInvalida = opcao != "1" && opcao != "2" && opcao != "3" && opcao != "4";
+            return opcaoInvalida;
+        }
+        static void RealizarCalculo(string operacao)
+        {
+            decimal resultado = 0;
+
+            Console.WriteLine("Digite o primeiro número");
+            decimal primeiroNumero = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Agora, o segundo número");
+            decimal segundoNumero = Convert.ToDecimal(Console.ReadLine());
+
+            if (operacao == "4" && segundoNumero == 0) Console.WriteLine("\nNão é possível dividir um número por zero :( Por favor, tente novamente");
+            else
+            {
+                switch (operacao)
+                {
+                    case "1":
+                        resultado = primeiroNumero + segundoNumero;
+                        break;
+                    case "2":
+                        resultado = primeiroNumero - segundoNumero;
+                        break;
+                    case "3":
+                        resultado = primeiroNumero * segundoNumero;
+                        break;
+                    case "4":
+                        resultado = primeiroNumero / segundoNumero;
+                        break;
+                }
+
+                Console.WriteLine("\nShow! Seu resultado é: " + resultado);
+            }
         }
     }
 }
